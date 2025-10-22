c8d538dcc3deffdccdb3db7428187f4ea82f2fe7
Silvia <ydril.linwelin@gmail.com>
2025-10-22 11:30:59 +0200

    add nuxt and fix the web

--- NetWithVueAndAspire.AppHost/Program.cs
+++ NetWithVueAndAspire.AppHost/Program.cs
@@ -3,4 +3,12 @@ using Projects;
 
 var builder = DistributedApplication.CreateBuilder(args);
 var api = builder.AddProject<NetWithVueAndAspire_Api>("Api");
+
+var web = builder.AddNpmApp("vue", "../NetWithVueAndAspire.Web","dev")
+    .WithNpmPackageInstallation()
+    .WithReference(api)
+    .WaitFor(api)
+    .WithHttpEndpoint(env: "PORT")
+    .WithExternalHttpEndpoints()
+    .PublishAsDockerFile();
 builder.Build().Run();
